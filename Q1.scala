06d3b900d4a30bff2dcd5cc8244ed92df3999eb8
raguram-24 <ram.ragu2404@gmail.com>
2023-08-08 06:45:01 +0530

    Scala Tutorial 7

---
+++ Q1.scala
@@ -0,0 +1,18 @@
+import scala.io.StdIn
+
+object Q1 {
+  def main(args: Array[String]): Unit = {
+    print("Enter the list of integers separated by commas: ")
+    val InputString = StdIn.readLine()
+    val InputList = InputString.split(",").map(_.trim.toInt).toList
+
+    val Output = filterEvenNumbers(InputList)
+    println("\n"+Output)
+
+  }
+
+  def filterEvenNumbers(numbers:List[Int]):List[Int] = {
+    numbers.filter(num => num%2 == 0)
+  }
+
+}
